60681627007c02013e54f4d05226f8f0def500ab
Valeria Epifania Flores Martinez <134737731+Vaalfinn@users.noreply.github.com>
2025-09-24 19:57:15 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "pwa_demo_cache_v1";
+const urlsToCache = ["/", "/index.html", "/app.js"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
